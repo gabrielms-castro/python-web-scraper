48d6e31fe422fc474f55c1f34b650dc3b43ce885
gabrielms-castro <gabrielms.castro@gmail.com>
2025-11-21 18:45:41 -0300

    add crawl_page recursion

--- crawl.py
+++ crawl.py
@@ -1,4 +1,6 @@
 from urllib.parse import urlparse, urljoin
+
+import requests
 from bs4 import BeautifulSoup
 import lxml
 
@@ -6,7 +8,7 @@ def normalize_url(url_string):
     if not url_string:
         return None
     parse_result = urlparse(url_string)
-    return f"{parse_result.netloc}{parse_result.path}"
+    return f"{parse_result.netloc}{parse_result.path}".lower()
 
 def get_h1_from_html(html):
     soup = BeautifulSoup(html, 'lxml')
@@ -49,10 +51,63 @@ def get_images_from_html(html, base_url):
     return urls
 
 def extract_page_data(html, page_url):
-    data = {}
-    data['url'] = page_url
-    data['h1'] = get_h1_from_html(html)
-    data['first_paragraph'] = get_first_paragraph_from_html(html)
-    data['outgoing_links'] = get_urls_from_html(html, page_url)
-    data['image_urls'] = get_images_from_html(html, page_url)
-    return data
+    return {
+        "url": page_url,
+        "h1": get_h1_from_html(html),
+        "first_paragraph": get_first_paragraph_from_html(html),
+        "outgoing_links": get_urls_from_html(html, page_url),
+        "image_urls": get_images_from_html(html, page_url),
+    }
+
+def get_html(url):
+    response = requests.get(
+        url=url,
+        headers={"User-Agent": "BootCrawler/1.0"}
+    )
+    response.raise_for_status()
+
+    if 'text/html' not in response.headers.get('content-type'):
+        raise Exception("Response content type is not 'text/html'")
+    
+    return response.text
+
+def safe_get_html(url):
+    try:
+        return get_html(url)
+    except Exception as e:
+        print(f"{e}")
+        return None
+
+def crawl_page(base_url, current_url=None, page_data=None):
+    if current_url is None:
+        current_url = base_url
+
+    if page_data is None:
+        page_data = {}
+
+    base_url_obj = urlparse(base_url)
+    current_url_obj = urlparse(current_url)
+    if current_url_obj.netloc != base_url_obj.netloc:
+        return page_data
+
+    normalized_url = normalize_url(current_url)
+    
+    if normalized_url in page_data:
+        print(f"Skipping already crawled: {normalized_url}")
+        return page_data
+
+    print(f"Scraping page: {current_url}")
+
+    html = safe_get_html(current_url)
+    if html is None:
+        return page_data
+    
+    page_info = extract_page_data(html, current_url)
+    page_data[normalized_url] = page_info
+
+    next_urls = get_urls_from_html(html, base_url)
+
+    for next_url in next_urls:
+        page_data = crawl_page(base_url, next_url, page_data)
+
+    return page_data
--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+import sys
+from crawl import crawl_page
+
 def main():
-    print("Hello from python-web-scraper!")
+    args = sys.argv
+    if len(args) < 2:
+        print("no website provided. Usage: uv run main.py <BASE_URL>")
+        sys.exit(1)
 
+    if len(args) > 2:
+        print("too many arguments provided")
+        sys.exit(1)
+    
+    BASE_URL = args[1]
+    print(f"[INFO] starting crawl of: {BASE_URL}")
+    try:
+        page_data = crawl_page(BASE_URL)
 
+        print(f"Found {len(page_data)} pages:")
+        for page in page_data.values():
+            print(f"- {page['url']}: {len(page['outgoing_links'])} outgoing links")
+
+    except Exception as err:
+        print(f"[ERROR] Error fetching {BASE_URL}")
 if __name__ == "__main__":
     main()
